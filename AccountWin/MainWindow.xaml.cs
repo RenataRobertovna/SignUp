ca54f6214a4fa153cde96c5684bc36c528e51824
anst <starininandrey@gmail.com>
2020-07-04 15:50:44 +0300

    Добавление работы с БД

--- AccountWin/MainWindow.xaml.cs
+++ AccountWin/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace AccountWin
 {
@@ -27,7 +28,34 @@ namespace AccountWin
 
         private void button_LogIn_Click(object sender, RoutedEventArgs e)
         {
+            var login = input_Login.Text;
+            var password = input_Password.Password;
+            
+            var host = "mysql11.hostland.ru";
+            var database = "host1323541_suptest2";
+            var port = "3306";
+            var username = "host1323541_itstep";
+            var pass = "269f43dc";
+            var ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
+            
+            MySqlConnection db = new MySqlConnection(ConnString);
+            db.Open();
 
+            string sql = $"SELECT login, pass FROM Account WHERE login = '{login}'";
+            MySqlCommand command = new MySqlCommand {Connection = db, CommandText = sql};
+            MySqlDataReader result = command.ExecuteReader();
+
+            if (!result.Read())
+            {
+                MessageBox.Show("Такого пользователя нет", "ОШИБКА", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                if (result.GetString(0) == login && result.GetString(1) == password)
+                {
+                    MessageBox.Show("Вход разрешён", "УСПЕХ", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
         }
 
         private void button_Clear_Click(object sender, RoutedEventArgs e)
